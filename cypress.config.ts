6b708fd9d62cf5d23c7765034ec205078913e6e4
gitkaviyarasi <kaviyarasikrishnannj@gmail.com>
2025-02-04 15:21:26 -0500

    inital commit

---
+++ cypress.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'cypress';
+import viteConfig from './vite.config';
+
+export default defineConfig({
+  component: {
+    port: 5173,
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+      viteConfig,
+    },
+  },
+
+  e2e: {
+    baseUrl: 'http://localhost:3001',
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+
+  },
+});
